b53ba465ef1ec9ef6f53ea3ab878db688976fe82
Alexandr Orel <4aorel@mail.ru>
2016-10-24 01:54:30 +0300

    django hello world

---
+++ ask_orel/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+#import datetime
+
+def hello_world(request):
+    #now = datetime.datetime.now()
+    #html = "<html><body>It is now %s.</body></html>" % now
+
+    data = '<p>Hello, world!</p>'\
+    '<p>'+ request.method + ' ' + request.path + '</p>'
+    return HttpResponse(data)
--- getting_started/gunicorn_hello_world.py
+++ getting_started/gunicorn_hello_world.py
@@ -1,17 +1,16 @@
+#gunicorn --config=getting_started/gunicorn_config.py ask_orel.wsgi
+
 #gunicorn --config=gunicorn_config.py gunicorn_hello_world:app
 #http://127.0.0.1:8081/lol.html?a=5&b=1
+
 def app(environ, start_response):
     print(environ['HTTP_HOST'] + environ['RAW_URI'])
 
-    http_host = environ['HTTP_HOST']
     request_method = environ['REQUEST_METHOD']
-    query_string = environ['QUERY_STRING']
-    server_protocol = environ['SERVER_PROTOCOL']
     raw_uri = environ['RAW_URI']
+
     data = 'Hello, world!\n'\
-    + request_method + ' ' + raw_uri + ' ' + server_protocol + '\n'
-    #+ 'Host: ' + http_host + '\n'\
-    #+ 'Query_string: ' + query_string + '\n'
+    + request_method + ' ' + raw_uri + '\n'
 
     status = '200 OK'
     response_headers = [
